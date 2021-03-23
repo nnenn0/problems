c33c955e4c01a1750699c4aed94dc68a16eaf1f5
nnenn0 <nnenn0memo@gmail.com>
2021-03-23 22:46:13 +0900

    add

---
+++ FFT/ALPC_F.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define rrep(i,a,b) for(int i = a; i >= b; i--)
+#define fore(i,a) for(auto& i : a)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(),(x).end()
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<long long, long long>;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+const int IINF = INT_MAX;
+const long long LINF = 1LL << 60;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+    cout << setprecision(15) << fixed;
+    int N, M; cin >> N >> M;
+    vector<ll> a(N), b(M);
+    rep(i, 0, N) cin >> a[i];
+    rep(i, 0, M) cin >> b[i];
+    auto fft = convolution(a, b);
+    fore(res, fft) cout << res << endl;
+    return 0;
+}
---
+++ FFT/ATC001C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define rrep(i,a,b) for(int i = a; i >= b; i--)
+#define fore(i,a) for(auto& i : a)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(),(x).end()
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<long long, long long>;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+const int IINF = INT_MAX;
+const long long LINF = 1LL << 60;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+    cout << setprecision(15) << fixed;
+    int N; cin >> N;
+    vector<ll> A(N+1, 0), B(N+1, 0);
+    rep(i, 0, N) cin >> A[i+1] >> B[i+1];
+    auto fft = convolution_ll(A, B);
+    rep(i, 0, 2*N+1) cout << fft[i] << endl;
+    return 0;
+}
